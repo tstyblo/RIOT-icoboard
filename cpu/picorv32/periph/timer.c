e640e05df9e92c3b1d16e419f5baf14fa6f12ec9
Tomas Styblo <tstyblo@gmail.com>
2020-09-08 21:26:38 +0200

    picorv32 - timer - add RDCYCLE instruction support - make ztimer (and xtimer) work

--- cpu/picorv32/periph/timer.c
+++ cpu/picorv32/periph/timer.c
@@ -20,6 +20,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define ENABLE_DEBUG 0
+#include "debug.h"
+#include "board.h"
+
 #include "cpu.h"
 #include "periph_cpu.h"
 #include "periph_conf.h"
@@ -30,11 +34,13 @@ static timer_isr_ctx_t timer_isr_ctx;
 
 int timer_init(tim_t dev, unsigned long freq, timer_cb_t cb, void *arg)
 {
+    DEBUG("timer.c - timer_init(): dev=%d, freq=%lu, cb=%p, arg=%p\n", dev, freq, cb, arg);
     if (dev != 0) {
         return -1;
     }
 
     if (freq != CLOCK_CORECLOCK) {
+        puts("ERROR: timer_init: freq != CLOCK_CORECLOCK\n");
         return -1;
     }
 
@@ -42,13 +48,14 @@ int timer_init(tim_t dev, unsigned long freq, timer_cb_t cb, void *arg)
     timer_isr_ctx.arg = arg;
 
     /* reset timer counter */
-    _picorv32_timer(0);
+    timer_clear(dev, 0);
 
     return 0;
 }
 
 int timer_set(tim_t dev, int channel, unsigned int timeout)
 {
+    DEBUG("timer.c - timer_set(): dev=%d, channel=%d, timeout=%u\n", dev, channel, timeout);
     if (dev != 0 || channel != 0) {
         return -1;
     }
@@ -60,13 +67,17 @@ int timer_set(tim_t dev, int channel, unsigned int timeout)
     return 0;
 }
 
-int timer_set_absolute(tim_t dev, int channel, __attribute__ ((unused)) unsigned int value)
+int timer_set_absolute(tim_t dev, int channel, unsigned int value)
 {
+    DEBUG("timer.c - timer_set_absolute(): dev=%d, channel=%d, value=%u\n", dev, channel, value);
     if (dev != 0 || channel != 0) {
         return -1;
     }
-    // unimplemented
-    return -1;
+
+    uint32_t now = timer_read(dev);
+    timer_set(dev, channel, abs(now-value));
+
+    return 0;
 }
 
 int timer_set_periodic(tim_t dev, int channel, __attribute__ ((unused)) unsigned int value,
@@ -75,6 +86,7 @@ int timer_set_periodic(tim_t dev, int channel, __attribute__ ((unused)) unsigned
     if (dev != 0 || channel != 0) {
         return -1;
     }
+
     // unimplemented
     return -1;
 }
@@ -95,8 +107,18 @@ unsigned int timer_read(tim_t dev)
     if (dev != 0) {
         return -1;
     }
-    // unimplemented
-    return -1;
+
+    uint64_t volatile rdcycle = _rdcycle_read();
+    uint32_t *p_res = (uint32_t *) &rdcycle;
+
+#if 0
+    uint8_t *p = (uint8_t *) &rdcycle;
+    for (int i = 0; i < 8; i++) {
+        printf("p[%d] p=%p v=0x%02X\n", i, p + i, *(p+i));
+    }
+#endif
+
+    return *p_res;
 }
 
 void timer_start(tim_t dev)
@@ -104,7 +126,8 @@ void timer_start(tim_t dev)
     if (dev != 0) {
         return;
     }
-    // unimplemented
+
+    // The timer device is always running.
 }
 
 void timer_stop(tim_t dev)
@@ -112,7 +135,10 @@ void timer_stop(tim_t dev)
     if (dev != 0) {
         return;
     }
-    // unimplemented
+
+    timer_clear(dev, 0);
+
+    // We can't really power down the timer device.
 }
 
 void timer_isr(void)
@@ -121,4 +147,8 @@ void timer_isr(void)
     if (timer_isr_ctx.cb) {
         timer_isr_ctx.cb(timer_isr_ctx.arg, 0);
     }
+    else {
+        puts("ERROR: timer callback is NULL");
+    }
 }
+
